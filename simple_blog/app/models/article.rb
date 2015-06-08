fc4b4724cb0a417c809d0ee69814080b16a18610
Rajendran <rajendran.r@cuelogic.co.in>
2015-06-08 13:40:41 +0530

    User added to the blogging system

--- simple_blog/app/models/article.rb
+++ simple_blog/app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
+  #Validations
   validates :title, :body, presence: true
+
+  #Associations
+  belongs_to :user
 
   def long_title
     "#{title} - #{published_at}"
